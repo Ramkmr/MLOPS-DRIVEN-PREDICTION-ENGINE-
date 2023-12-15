aa3bd58064af8ab824704a3e3a429db342c7a8cb
Ramkmr <rmkmr211997@gmail.com>
2023-12-15 23:14:22 +0530

    Data Transformation completed

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-"""from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
-from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+"""from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline """
 
 
@@ -30,7 +30,7 @@ except Exception as e:
 
 
 
-"""STAGE_NAME = "Data Transformation stage"
+STAGE_NAME = "Data Transformation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataTransformationTrainingPipeline()
@@ -43,7 +43,7 @@ except Exception as e:
 
 
 
-STAGE_NAME = "Model Trainer stage"
+"""STAGE_NAME = "Model Trainer stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = ModelTrainerTrainingPipeline()
--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -1,6 +1,8 @@
 from mlProject.constants import *
 from mlProject.utils.common import read_yaml, create_directories
-from mlProject.entity.config_entity import (DataIngestionConfig,DataValidationConfig)
+from mlProject.entity.config_entity import (DataIngestionConfig,
+                                            DataValidationConfig,
+                                            DataTransformationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -49,7 +51,7 @@ class ConfigurationManager:
     
 
 
-    """def get_data_transformation_config(self) -> DataTransformationConfig:
+    def get_data_transformation_config(self) -> DataTransformationConfig:
         config = self.config.data_transformation
 
         create_directories([config.root_dir])
@@ -63,7 +65,7 @@ class ConfigurationManager:
     
 
 
-    def get_model_trainer_config(self) -> ModelTrainerConfig:
+    """def get_model_trainer_config(self) -> ModelTrainerConfig:
         config = self.config.model_trainer
         params = self.params.ElasticNet
         schema =  self.schema.TARGET_COLUMN
